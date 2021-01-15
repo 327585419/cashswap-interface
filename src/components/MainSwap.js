6e1574a615c651d0e5f66aba496807fd1fd70058
DP <websiteservices@protonmail.com>
2021-01-15 07:24:05 -0500

    constants

--- src/components/MainSwap.js
+++ src/components/MainSwap.js
@@ -3,7 +3,8 @@ import { Container, Row, Col } from 'reactstrap';
 import { InputGroup, InputGroupAddon, Button, Input } from 'reactstrap';
 import { Media, Form, FormGroup, Label } from 'reactstrap';
 
-
+const BCHSLPprice = 400000
+const BCHUSDprice = 530
 
 class SwapForm extends React.Component {
   constructor(props) {
@@ -14,22 +15,15 @@ class SwapForm extends React.Component {
   }
 
   handleChange(e) {
-    this.setState({ bch: e.target.value, slp: e.target.value / 400000, bchusd: e.target.value * 530, slpusd: e.target.value * 530 / 400000 });
+    this.setState({ bch: e.target.value, slp: e.target.value / BCHSLPprice, bchusd: e.target.value * BCHUSDprice, slpusd: e.target.value * BCHUSDprice / BCHSLPprice });
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    if (this.state.text.length === 0) {
+    if (this.state.slp === 0) {
       return;
     }
-    const newItem = {
-      text: this.state.text,
-      id: Date.now()
-    };
-    this.setState(state => ({
-      items: state.items.concat(newItem),
-      text: ''
-    }));
+
   }
 
   render() {
@@ -41,7 +35,7 @@ class SwapForm extends React.Component {
                     You are exchanging:
                   <Col sm={10}>
                     <Input type="text" name="BCHamount" id="BCHamount" onChange={this.handleChange} value={this.state.bch} bsSize="lg" /><Label for="BCHamount" sm={2} size="lg">BCH</Label>
-                    <Input type="text" name="BCHUSDamount" id="BCHUSDamount" bsSize="lg" value={this.state.bchusd} /><Label for="BCHUSDamount" sm={2} size="lg">USD</Label>
+                    <Input type="text" name="BCHUSDamount" id="BCHUSDamount" bsSize="lg" value={this.state.bchusd} disabled /><Label for="BCHUSDamount" sm={2} size="lg">USD</Label>
                   </Col>
 
                 </FormGroup>
@@ -49,8 +43,8 @@ class SwapForm extends React.Component {
                 <FormGroup row>
                     You will receive:
                   <Col sm={10}>
-                    <Input type="text" name="SLPamount" id="SLPamount" bsSize="lg" value={this.state.slp} /><Label for="SLP" sm={2} size="lg">SLP</Label>
-                    <Input type="text" name="SLPUSDamount" id="SLPUSDamount" bsSize="lg" value={this.state.slpusd} /><Label for="SLPUSDamount" sm={2} size="lg">USD</Label>
+                    <Input type="text" name="SLPamount" id="SLPamount" bsSize="lg" value={this.state.slp} disabled /><Label for="SLP" sm={2} size="lg">SLP</Label>
+                    <Input type="text" name="SLPUSDamount" id="SLPUSDamount" bsSize="lg" value={this.state.slpusd} disabled /><Label for="SLPUSDamount" sm={2} size="lg">USD</Label>
                   </Col>
                 </FormGroup>
                 <Button>Exchange</Button>
