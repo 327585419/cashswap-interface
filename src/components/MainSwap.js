eb48ad8809bb117a703d1fb98b22b808a2569fc4
DP <websiteservices@protonmail.com>
2021-01-18 10:32:36 -0500

    UI

--- src/components/MainSwap.js
+++ src/components/MainSwap.js
@@ -3,24 +3,41 @@ import { Container, Row, Col } from 'reactstrap';
 import { InputGroup, InputGroupAddon, Button, Input } from 'reactstrap';
 import { Media, Form, FormGroup, Label, InputGroupText } from 'reactstrap';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Spinner } from 'reactstrap';
 import './MainSwap.css';
+import { ArrowDown } from 'react-feather'
+import {swap} from '../service/contract.js'
 
 const BCHSLPprice = 400000
 const BCHUSDprice = 530
+
+function PresentResultOfSwap(props){
+    if (props.swapStatus === 'ready'){ return (<p>PresentResultOfSwap</p>)}
+    if (props.swapStatus === 'working'){ return (<Spinner size="sm" color="secondary" />)}
+    return (<></>)
+  }
 
 class SwapForm extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = { slp: 0.0, bch: 0.0, slpusd: 0.0, bchusd: 0.0, privateKey: '',
-        isModalOpen: false, setModal: false };
+        isModalOpen: false, setModal: false,
+        swapStatus: 'ready'};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.toggleModal = this.toggleModal.bind(this);
     this.handleChangePrivateKey = this.handleChangePrivateKey.bind(this);
     this.handleSubmitPrivateKey = this.handleSubmitPrivateKey.bind(this);
   }
+
+  handleInputChange(event) {
+      const target = event.target;
+      const value = target.type === 'checkbox' ? target.checked : target.value;
+      const name = target.name;
+      this.setState({
+        [name]: value    });
+    }
 
   handleChange(e) {
     this.setState({ bch: e.target.value, slp: e.target.value / BCHSLPprice, bchusd: e.target.value * BCHUSDprice, slpusd: e.target.value * BCHUSDprice / BCHSLPprice });
@@ -34,6 +51,7 @@ class SwapForm extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
+
     if (this.state.slp === 0 ) {
       return;
     }
@@ -41,6 +59,15 @@ class SwapForm extends React.Component {
         this.toggleModal();
       return;
     }
+
+    this.setState((state)=>{return {swapStatus : 'working'}});
+    if ( swap(this.state.slp, this.state.bch) === false ){
+        this.setState((state)=>{return {swapStatus : 'swap failed'}});
+    }
+    if ( swap(this.state.slp, this.state.bch) === true ){
+        this.setState((state)=>{return {swapStatus : 'swap succeeded'}});
+    }
+
   }
 
   handleChangePrivateKey(e) {
@@ -52,17 +79,18 @@ class SwapForm extends React.Component {
 
   handleSubmitPrivateKey(e) {
     e.preventDefault();
+    this.handleSubmit(e) ;
   }
 
   render() {
       return (
-
               <div id="mainDiv">
+              
               <Row><Col><h4 className="SwapFormTitle">Swap</h4></Col><Col xs="auto"></Col></Row>
               <Form>
                 <FormGroup row className="formGroupFrom">
                     <Row><Col>You are exchanging:</Col>
-                  <Col sm={10}>
+                  <Col sm={9}>
                         <Row><InputGroup>
                     <Input type="number" name="BCHamount" id="BCHamount" onChange={this.handleChange} value={this.state.bch} bsSize="lg" />
                         <InputGroupAddon addonType="append">
@@ -79,15 +107,11 @@ class SwapForm extends React.Component {
                 </FormGroup>
 
 
-<Media>
-  <Media left>
-    <Media object data-src="../media/arrow-down-grey.svg/64x64" alt="arrow down" />
-  </Media>
-</Media>
+                <ArrowDown size="18" color={'black'} />
 
                 <FormGroup row className="formGroupTo">
-                    <Row><Col>You will receive:</Col>
-                  <Col sm={10}>
+                    <Row><Col xs="auto"></Col><Col>You will receive:</Col>
+                  <Col sm={9}>
                         <Row><InputGroup>
                     <Input type="number" name="SLPamount" id="SLPamount" onChange={this.handleChange} value={this.state.slp} bsSize="lg" disabled />
                         <InputGroupAddon addonType="append">
@@ -103,19 +127,20 @@ class SwapForm extends React.Component {
 
                 </FormGroup>
 
-                <Button onClick={this.handleSubmit} className="SwapButton">Swap</Button>
+                <Button onClick={this.handleSubmit} className="SwapButton">Start Swap!</Button>
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                   <ModalHeader toggle={this.toggleModal}>Private key needed for the swap</ModalHeader>
                   <ModalBody>
-                    Paste your private key string here:
-                    <Input type="text" name="PrivateKey" id="PrivateKey"  onChange={this.handleChangePrivateKey} value={this.state.privateKey} bsSize="lg" />
+
+                    <Input placeholder="Paste your private key string here" type="text" name="PrivateKey" id="PrivateKey"  onChange={this.handleChangePrivateKey} value={this.state.privateKey} bsSize="lg" />
                   </ModalBody>
                   <ModalFooter>
-                    <Button color="primary" onClick={this.handleSubmitPrivateKey}>Do Something</Button>{' '}
+                    <Button color="primary" onClick={this.handleSubmitPrivateKey}>OK</Button>{' '}
                     <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
                   </ModalFooter>
                 </Modal>
               </Form>
+              <PresentResultOfSwap swapStatus={this.swapStatus} />
               </div>
       );
   }
